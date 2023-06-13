7011895ac3ca36ff3d1fac49cabf660a7eca4147
marcop96 <55959162+marcop96@users.noreply.github.com>
2023-06-13 18:19:39 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+const id = ref(0);
+const newTask = ref("");
+const tasks = ref([]);
+function addTask() {
+  tasks.value.push({ id: id.value++, text: newTask.value });
+}
+newTask.value = "";
+</script>
+
+<template>
+  <input v-model="newTask" />
+
+  <button @click="addTask" :key="newTask.id">Add task</button>
+
+  <li v-for="task in tasks">
+    {{ task.value }}
+  </li>
+</template>
+
+<style scoped></style>
