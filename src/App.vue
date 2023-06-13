08a791837f84fb3bf4423b12b1ac7713dd385d93
marcop96 <55959162+marcop96@users.noreply.github.com>
2023-06-13 18:40:37 +0200

    function: remove task

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@ function addTask() {
   tasks.value.push({ id: id.value++, text: newTask.value });
   newTask.value = "";
 }
+function deleteTask(task) {
+  tasks.value = tasks.value.filter((t) => t != task);
+  console.log("delete task");
+}
 </script>
 
 <template>
@@ -14,7 +18,9 @@ function addTask() {
 
   <button @click="addTask" :key="newTask.id">Add task</button>
 
-  <li v-for="task in tasks">{{ task.text }} <button>X</button></li>
+  <li v-for="task in tasks">
+    {{ task.text }} <button @click="deleteTask(task)">X</button>
+  </li>
 </template>
 
 <style scoped></style>
