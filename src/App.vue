d3919a3591bfec755a13551c205faed410775ea1
marcop96 <55959162+marcop96@users.noreply.github.com>
2023-06-14 21:20:52 +0200

    delete unnecessary style files

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,26 @@ function validateAddtask() {
 </script>
 
 <template>
-  <input
-    className="border-black border-2 outline-red-500 rounded-full"
-    v-model="newTask"
-  />
-  <button
-    className="bg-green-600 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full"
-    @click="validateAddtask"
-    :key="newTask.id"
-  >
-    Add task
-  </button>
-  <li v-for="task in tasks" :key="task.id">
-    <input type="checkbox" v-model="task.done" />
-    <span :class="{ done: task.done }">{{ task.text }}</span>
-    <button @click="deleteTask(task)">X</button>
-  </li>
+  <div class="div">
+    <input
+      class="border-black border-2 outline-red-500 rounded-full"
+      v-model="newTask"
+    />
+    <button
+      class="bg-green-600 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full"
+      @click="validateAddtask"
+      :key="newTask.id"
+    >
+      Add task
+    </button>
+  </div>
+  <div class="w-full">
+    <li v-for="task in tasks" :key="task.id">
+      <input type="checkbox" v-model="task.done" />
+      <span :class="{ done: task.done }">{{ task.text }}</span>
+      <button class="border-2 border-black" @click="deleteTask(task)">X</button>
+    </li>
+  </div>
 </template>
 
 <style scoped>
