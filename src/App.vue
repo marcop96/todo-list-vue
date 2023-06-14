4ecb17652fe8ac99499ebbe10780a0d8b0a45b1c
marcop96 <55959162+marcop96@users.noreply.github.com>
2023-06-14 08:00:36 +0200

    validates before adding task

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,24 @@ function addTask() {
 }
 function deleteTask(task) {
   tasks.value = tasks.value.filter((t) => t != task);
-  console.log("delete task");
+}
+function validateAddtask() {
+  if (newTask.value == "") {
+    console.log("input vacio");
+  } else {
+    addTask();
+  }
 }
 </script>
 
 <template>
   <input v-model="newTask" />
 
-  <button @click="addTask" :key="newTask.id">Add task</button>
+  <button @click="validateAddtask" :key="newTask.id">Add task</button>
 
   <li v-for="task in tasks">
-    {{ task.text }} <button @click="deleteTask(task)">X</button>
+    {{ task.text }}
+    <button @click="deleteTask(task)">X</button>
   </li>
 </template>
 
